998df1e94969f35796b6323a6cad1309a91aedb0
Emmanuel James <chiderajames55@gmail.com>
2023-02-01 19:12:36 -0800

    add Private instance attribute

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,46 +1,47 @@
 #!/usr/bin/python3
-
-"""
-    Project on python classes and objects
-    Rectangle class #1
+"""Rectangle module.
+This module contains a class that defines a rectangle.
 """
 
 
-class Rectangle:
-    """ A class representing a rectangle """
+class Rectangle():
+    """Defines a rectangle."""
+
     def __init__(self, width=0, height=0):
-        """ constructor
+        """Sets the necessary attributes for the Rectangle object.
         Args:
-            width: width of the rectangle
-            height: height of the rectangle
+            width (int): the width of the rectangle.
+            height (int): the height of the rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """ getter method for width private instance attribute """
+        """Get or set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ setter method for width private instance attribute """
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if value < 0:
-            raise ValueError('width must be >= 0')
-        self.__width = value
+        if type(value) is int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
 
     @property
     def height(self):
-        """ getter method for height private instance attribute """
+        """Get or set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ setter method for height private instance attribute """
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-        if value < 0:
-            raise ValueError('height must be >= 0')
-        self.__height = value
+        if type(value) is int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
